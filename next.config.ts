f504b9bcb40da7ca934a3f3409269c03b93f4754
zkjon <focusyts@gmail.com>
2025-06-04 07:46:24 +0000

    https://apis.es/_old/wp-content/uploads/2017/01/logo-apis2-footer.png
    reemplazalo tu

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'apis.es',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,12 +4,11 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://placehold.co/104x40.png" // Reemplaza esto con la ruta a tu logo real
+      src="https://apis.es/_old/wp-content/uploads/2017/01/logo-apis2-footer.png"
       alt="Logo de la Empresa - AlmacenControl"
       width={104} // Ancho deseado para mostrar el logo
       height={40}  // Alto deseado para mostrar el logo
       className={cn(className)}
-      data-ai-hint="company logo apis" // Indicador para el reemplazo de imagen
       priority // Prioriza la carga del logo, ya que es probable que esté en el LCP
     />
   );
